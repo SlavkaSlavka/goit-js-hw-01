5836d80d56b9220c802ea0139d4e5d7fc332cd32
SlavkaSlavka <slavkabest1993@gmail.com>
2021-02-20 12:21:46 +0200

    тернарник

--- JS/task-2.js
+++ JS/task-2.js
@@ -10,9 +10,5 @@
 
 const total = 100;
 const ordered = 130;
-
-if (ordered > total) {
-  console.log('На складе недостаточно товаров!');
-} else {
-  console.log("Заказ оформлен, с вами свяжется менеджер");
-}
+const message = ordered > total ? 'На складе недостаточно товаров!' : "Заказ оформлен, с вами свяжется менеджер";
+console.log(message);
